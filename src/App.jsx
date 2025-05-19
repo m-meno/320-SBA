4d40faeb8cdd6ea5eb55c2ae17ff4deaa073837a
m-meno <mackenzie.meno@gmail.com>
2025-05-19 10:55:29 -0600

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState, useEffect } from 'react'
+import './App.css';
+import {Routes, Route} from "react-router-dom";
+
+function App() {
+  
+
+  return (
+    <>
+    <Routes>
+
+
+    </Routes>
+    </>
+  )
+}
+
+export default App
